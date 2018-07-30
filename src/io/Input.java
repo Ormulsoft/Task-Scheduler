343d07d8bd668592d5f639f118c3424c02ea16c3
Shane Barboza <shane.barboza@gmail.com>
2018-07-31 01:20:06 +1200

    Input file is read, unnecessary lines removed.

--- src/io/Input.java
+++ src/io/Input.java
@@ -1,10 +1,54 @@
 package io;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import grph.Grph;
 
 public class Input {
 
     public static Grph readDotInput(String path) {
+       
+    	File file = new File(path);
+        Scanner input = null;
+        
+		try {
+			input = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			System.out.println("File was not found!");
+		}
+		
+        List<String> list = new ArrayList<String>();
+
+        // Read .DOT file line by line
+        while (input.hasNextLine()) {
+        	
+        	String currentLine = input.nextLine();
+        	
+        	// Only add if it doesn't contain '{', '}', or only whitespace.
+        	if ((currentLine.indexOf('{') == -1) && (currentLine.indexOf('}') == -1)) {
+        		if (!currentLine.trim().isEmpty()) {
+        			
+        		    // String is not empty and not just whitespace
+        			list.add(currentLine);
+        		} 
+        		
+            	
+        	}
+            
+        }
+        
+        
+        
+        
+        for (String l : list) {
+        	System.out.println(l);
+        	System.out.println("end");
+        }
+
         return null;
     }
 
--- src/io/Main.java
+++ src/io/Main.java
@@ -1,20 +1,26 @@
 package io;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 import grph.Grph;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        String inputPath = "";
+        String inputPath = "Nodes_7_OutTree.dot";
         String outputPath = "";
         int cores = 1;
 
         
         
-        Grph in = Input.readDotInput(inputPath);
-        Grph out = alg.Algorithm.runAlg(in,cores);
-        Output.export(out, outputPath);
+        
+		Grph in = Input.readDotInput(inputPath);
+		Grph out = alg.Algorithm.runAlg(in,cores);
+		Output.export(out, outputPath);
+        
 
 
     }
