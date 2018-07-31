a918d844e0b83e0c01ef84c02e2a1c96de029d75
Your Name <nikhilreddy24@hotmail.com>
2018-07-31 19:35:18 +1200

    Created Implemenation of Basic Edge & Vertex classes which is going to be used as a wrapper for the grph library object

--- src/io/Edge.java
+++ src/io/Edge.java
@@ -1,5 +1,14 @@
 package io;
 
 public class Edge {
+    public int _weight;
+    public Vertex _src;
+    public Vertex _dest;
+
+    Edge(int weight,Vertex src,Vertex dest) {
+        _weight = weight;
+        _src = src;
+        _dest = dest;
+    }
 
 }
--- src/io/Vertex.java
+++ src/io/Vertex.java
@@ -1,5 +1,43 @@
 package io;
 
-public class Vertex {
+import java.util.ArrayList;
 
+public class Vertex {
+    private int _index;
+    private boolean _start;
+    private boolean _finish;
+    private ArrayList<Edge> EdgeSet;
+
+    Vertex(int _name,boolean start,boolean finish) {
+        _index = _name;
+        _start =start;
+        _finish = finish;
+    }
+
+    public ArrayList<Edge> getEdgeSet() {
+        return EdgeSet;
+    }
+
+    public int get_index() {
+        return _index;
+    }
+
+    public boolean is_finish() {
+        return _finish;
+    }
+
+    public boolean is_start() {
+        return _start;
+    }
+
+    public ArrayList<Vertex> getAllConnectedVertexs() {
+        ArrayList<Vertex> _output = new ArrayList<Vertex>();
+        for(Edge e:EdgeSet) {
+            if(e._src == this) {
+                _output.add(e._dest);
+            }
+
+        }
+        return  _output;
+    }
 }
