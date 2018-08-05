00da545eaa10e9a371a524405937a7f07bf68b1f
mfrost433 <mfrost433@gmail.com>
2018-08-05 22:28:37 +1200

    completed commenting and code cleanup for submission 1

--- src/alg/cost/TestCostFunction.java
+++ src/alg/cost/TestCostFunction.java
@@ -5,6 +5,7 @@ import util.ScheduleGrph;
 
 /**
  * A basic cost function that assigns the end time as the cost.
+ * 
  * @author Matt
  *
  */
@@ -13,68 +14,47 @@ public class TestCostFunction implements CostFunction {
 	ScheduleGrph input;
 
 	public TestCostFunction(ScheduleGrph input) {
-		this.input = input;		
+		this.input = input;
 	}
 
 	public void applyCost(PartialScheduleGrph g) {
 
-
 	}
 
-	public void applyCost(PartialScheduleGrph g, int addedVertex) {		
+	public void applyCost(PartialScheduleGrph g, int addedVertex) {
 
 		int maxFinish = 0;
-		for(int i : g.getVertices()) {
+		for (int i : g.getVertices()) {
 			// get the end time from the highest start time + weight combination
 			int val = (int) g.getVertexStartProperty().getValue(i) + (int) g.getVertexWeightProperty().getValue(i);
-			if(val > maxFinish) {
+			if (val > maxFinish) {
 				maxFinish = val;
-			}			
-		}		
-		
-		g.setScore(maxFinish + getBottomLevel(addedVertex));
+			}
+		}
+
+		g.setScore(maxFinish + getComputationalBottomLevel(addedVertex));
 	}
 
-	
-	private int getBottomLevel(int addedVertex) {
-		
-		if(input.getOutEdgeDegree(addedVertex) > 0) {
-			int max = (int)(input.getVertexWeightProperty().getValue(addedVertex) + input.getVertexStartProperty().getValue(addedVertex));
-			for( int i : input.getOutNeighbors(addedVertex)) {
-				int current = (int)(getBottomLevel(i) + input.getVertexWeightProperty().getValue(i));
-				if(max < current) {
+	/**
+	 * Gets the Computational bottom level value for an added vertex (ie. the
+	 * longest path of task weights not including dependency edge weights)
+	 */
+	private int getComputationalBottomLevel(int addedVertex) {
+
+		if (input.getOutEdgeDegree(addedVertex) > 0) {
+			int max = (int) (input.getVertexWeightProperty().getValue(addedVertex)
+					+ input.getVertexStartProperty().getValue(addedVertex));
+			for (int i : input.getOutNeighbors(addedVertex)) {
+				int current = (int) (getComputationalBottomLevel(i) + input.getVertexWeightProperty().getValue(i));
+				if (max < current) {
 					max = current;
 				}
-			}	
+			}
 			return max;
-		}else {
-			return (int)( input.getVertexStartProperty().getValue(addedVertex) +input.getVertexWeightProperty().getValue(addedVertex));
+		} else {
+			return (int) (input.getVertexStartProperty().getValue(addedVertex)
+					+ input.getVertexWeightProperty().getValue(addedVertex));
 		}
 	}
-	
-	public int  getComputationalBottomLevel(int addedvertex) {
-		boolean holder = true;
-		int bottomlevel = 0;
-		while (holder) {
-			int[] a = input.getOutEdges(addedvertex).toIntArray();
-			int max = 0;
-			if(input.getOutEdgeDegree(addedvertex) == 0) {
-				holder = false;
-			}
-			int finalotherVertex = 0;
-			for(int edge: a) {
-				int othervertex = input.getTheOtherVertex(edge,addedvertex);
-				if(input.getVertexWeightProperty().getValueAsInt(othervertex) > max) {
-					max = input.getVertexWeightProperty().getValueAsInt(othervertex);
-					finalotherVertex = othervertex;
-				}
-
-			}
-			bottomlevel = bottomlevel+max;
-			addedvertex = finalotherVertex;
-		}
-		return  bottomlevel;
-	}
-
 
 }
