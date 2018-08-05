4c0deeae51dd7390a479a5bfe62983c5f2b9c076
mfrost433 <mfrost433@gmail.com>
2018-08-05 13:27:07 +1200

    added cost func

--- src/alg/cost/TestCostFunction.java
+++ src/alg/cost/TestCostFunction.java
@@ -5,7 +5,15 @@ import util.PartialScheduleGrph;
 public class TestCostFunction implements CostFunction {
 
 	public void applyCost(PartialScheduleGrph g) {
-		g.setScore(1);
+		int max = 0;
+		for(int i : g.getVertices()) {
+			int val = (int) g.getVertexStartProperty().getValue(i) + (int) g.getVertexWeightProperty().getValue(i);
+			if(val > max) {
+				max = val;
+			}
+			
+		}
+		g.setScore(max);
 
 	}
 
