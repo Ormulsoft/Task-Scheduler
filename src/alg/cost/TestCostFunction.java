b36c4503d5b7cc3462e7e2c4012385c623378248
Eugene-Bulog <eugene.bulog@gmail.com>
2018-08-06 16:37:31 +1200

    Added idle time fit function

--- src/alg/cost/TestCostFunction.java
+++ src/alg/cost/TestCostFunction.java
@@ -1,7 +1,14 @@
 package alg.cost;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+import grph.properties.NumericalProperty;
+import toools.collections.primitive.LucIntSet;
 import util.PartialScheduleGrph;
 import util.ScheduleGrph;
+
 
 /**
  * A basic cost function that assigns the end time as the cost.
@@ -55,6 +62,68 @@ public class TestCostFunction implements CostFunction {
 			return (int) (input.getVertexStartProperty().getValue(addedVertex)
 					+ input.getVertexWeightProperty().getValue(addedVertex));
 		}
+	}
+	
+	
+	/**
+	 * Returns the FIT(s) function representing the idle time bound of a parital schedule.
+	 * @param sched The partial schedule whose bound is to be calculated
+	 * @param numProcessors The number of processors being used for task allocation
+	 * @return The idle time bound of this schedule
+	 */
+	public static double getIdleTimeFit(PartialScheduleGrph sched, int numProcessors) {
+		int totalIdle = 0;
+		int totalWeight = 0;
+		NumericalProperty vertProcs = sched.getVertexProcessorProperty();
+		final NumericalProperty vertStarts = sched.getVertexStartProperty(); 
+		NumericalProperty vertWeights = sched.getVertexWeightProperty();
+		LucIntSet taskIDs = sched.getVertices();
+		
+		// Create a list of lists, each list relates to a processor and stores the tasks on that processor
+		// for sorting later
+		ArrayList<ArrayList<Integer>> processors = new ArrayList<ArrayList<Integer>>();
+		
+		for (int i = 0; i < numProcessors; i++) {
+			processors.add(new ArrayList<Integer>());
+		}
+		
+		// Add each task to the list related to the relevant processor
+		for (int task : taskIDs) {
+			processors.get(vertProcs.getValueAsInt(task) - 1).add(task);
+			totalWeight += vertWeights.getValueAsInt(task);
+		}
+		log.debug("totalweight" + totalWeight);
+		
+		// Add idle time of each processor to total
+		for (int i = 0; i < numProcessors; i++) {
+			
+			ArrayList<Integer> list = processors.get(i);
+			
+			// Sort tasks based on start time
+			Collections.sort(list, new Comparator<Integer>(){
+
+				public int compare(Integer o1, Integer o2) {
+					return ((Integer)(vertStarts.getValueAsInt(o1))).compareTo((Integer)(vertStarts.getValueAsInt(o2)));
+				}
+			
+			});
+			log.debug(list);
+			int finishTime = 0;
+			
+			// If there is a gap between a previous task and this one, add the gap to idletime.
+			for (int task : list) {
+				if (vertStarts.getValueAsInt(task) > finishTime) {
+					totalIdle += vertStarts.getValueAsInt(task) - finishTime;
+				}
+				finishTime = vertStarts.getValueAsInt(task) + vertWeights.getValueAsInt(task);
+				log.debug("task " + task + " proc " + i + " finishTime " + finishTime);
+			}
+			
+		}
+		
+		log.debug("total idle " + totalIdle);
+		
+		return ((totalIdle + totalWeight) / (double)numProcessors);
 	}
 
 }
