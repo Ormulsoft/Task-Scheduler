738c267c992f8cccd6292a5f9f7ba401dac42b0b
Eugene Bulog <ebul920@en-369800.uoa.auckland.ac.nz>
2018-08-20 11:17:00 +1200

    Finished clean javadocs for util package

--- src/util/StaticUtils.java
+++ src/util/StaticUtils.java
@@ -1,12 +1,25 @@
 package util;
 
+/**
+ * A class for various useful but miscellaneous static utility methods
+ * @author Various, OrmulSoft
+ *
+ */
 public class StaticUtils {
-
+	
+	/**
+	 * Gets the current remaining memory of the runtime
+	 * @return Remaining memory in MB
+	 */
 	public static long getRemainingMemory() {
 		long allocatedMemory = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory());
 		return Runtime.getRuntime().maxMemory() - allocatedMemory;
 	}
 	
+	/**
+	 * Gets the current used memory of the runtime
+	 * @return Used memory in MB
+	 */
 	public static double getUsedMemory(){
 		return ((Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (1024.0 * 1024.0 ));
 		
